82fdf629861be19b242cda41238d1858bab43295
Ste[PH]ane <steph.guery@gmail.com>
2023-03-29 21:58:13 +0200

    Page blog-list

--- blog.php
+++ blog.php
@@ -1,10 +1,123 @@
-<?php
-$page = "blog";
-include 'header.php';
-include 'nav.php'
-;?>
-    <header class="header__page">
-        <h1>work in progress</h1>
-    </header>
+<?php $page = "blog";include 'header.php';include 'nav.php';?>
+
+<header class="header__page">
+    <h1>work in progress</h1>
+</header>
+<main>
+    <!-- Classe 'container' pour installer la grille bootstrap -->
+    <div class="container">
+        <!-- Je veux sur une ligne 3 colonnes -->
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card">
+                <img src="/images/img_blog.jpg" alt="des oeufs sur le plat" class="card-img-top">
+
+                    <div class="card-body">
+                        <div class="card-title">Test titre card blog</div>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Date</h6>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, nesciunt provident quisquam nihil natus beatae dignissimos necessitatibus vero! Et repellendus magnam voluptatibus dignissimos earum quasi atque optio deleniti? Quod.</p>
+                        <a href="#" class="card-link">Lien vers l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="/images/img_blog.jpg" alt="des oeufs sur le plat" class="card-img-top">
+
+                    <div class="card-body">
+                        <div class="card-title">Test titre card blog</div>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Date</h6>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, nesciunt provident quisquam nihil natus beatae dignissimos necessitatibus vero! Et repellendus magnam voluptatibus dignissimos earum quasi atque optio deleniti? Quod.</p>
+                        <a href="#" class="card-link">Lien vers l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="/images/img_blog.jpg" alt="des oeufs sur le plat" class="card-img-top">
+
+                    <div class="card-body">
+                        <div class="card-title">Test titre card blog</div>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Date</h6>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, nesciunt provident quisquam nihil natus beatae dignissimos necessitatibus vero! Et repellendus magnam voluptatibus dignissimos earum quasi atque optio deleniti? Quod.</p>
+                        <a href="#" class="card-link">Lien vers l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="/images/img_blog.jpg" alt="des oeufs sur le plat" class="card-img-top">
+
+                    <div class="card-body">
+                        <div class="card-title">Test titre card blog</div>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Date</h6>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, nesciunt provident quisquam nihil natus beatae dignissimos necessitatibus vero! Et repellendus magnam voluptatibus dignissimos earum quasi atque optio deleniti? Quod.</p>
+                        <a href="#" class="card-link">Lien vers l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="/images/img_blog.jpg" alt="des oeufs sur le plat" class="card-img-top">
+
+                    <div class="card-body">
+                        <div class="card-title">Test titre card blog</div>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Date</h6>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, nesciunt provident quisquam nihil natus beatae dignissimos necessitatibus vero! Et repellendus magnam voluptatibus dignissimos earum quasi atque optio deleniti? Quod.</p>
+                        <a href="#" class="card-link">Lien vers l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="/images/img_blog.jpg" alt="des oeufs sur le plat" class="card-img-top">
+
+                    <div class="card-body">
+                        <div class="card-title">Test titre card blog</div>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Date</h6>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, nesciunt provident quisquam nihil natus beatae dignissimos necessitatibus vero! Et repellendus magnam voluptatibus dignissimos earum quasi atque optio deleniti? Quod.</p>
+                        <a href="#" class="card-link">Lien vers l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="/images/img_blog.jpg" alt="des oeufs sur le plat" class="card-img-top">
+
+                    <div class="card-body">
+                        <div class="card-title">Test titre card blog</div>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Date</h6>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, nesciunt provident quisquam nihil natus beatae dignissimos necessitatibus vero! Et repellendus magnam voluptatibus dignissimos earum quasi atque optio deleniti? Quod.</p>
+                        <a href="#" class="card-link">Lien vers l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="/images/img_blog.jpg" alt="des oeufs sur le plat" class="card-img-top">
+
+                    <div class="card-body">
+                        <div class="card-title">Test titre card blog</div>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Date</h6>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, nesciunt provident quisquam nihil natus beatae dignissimos necessitatibus vero! Et repellendus magnam voluptatibus dignissimos earum quasi atque optio deleniti? Quod.</p>
+                        <a href="#" class="card-link">Lien vers l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="/images/img_blog.jpg" alt="des oeufs sur le plat" class="card-img-top">
+
+                    <div class="card-body">
+                        <div class="card-title">Test titre card blog</div>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Date</h6>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur excepturi, nesciunt provident quisquam nihil natus beatae dignissimos necessitatibus vero! Et repellendus magnam voluptatibus dignissimos earum quasi atque optio deleniti? Quod.</p>
+                        <a href="#" class="card-link">Lien vers l'article</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 
 <?php include 'footer.php';?>
