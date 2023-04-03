a004bd89d14cc8c6ccd90235b04e18fbaf518b84
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2023-04-03 09:24:35 +0200

    Fixed VS Sticky

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <!-- Barre de navigation avec à gauche le menu et à droite le bouton de prise de RDV-->
 
-<nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
+<nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid px-4">
         <a class="navbar-brand" href="/priseRDV.php">Prenez un RDV</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
