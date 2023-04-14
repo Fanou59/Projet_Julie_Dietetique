864dbc43afb9c7817ea1e11684a48879abd60ca6
Ste[PH]ane <steph.guery@gmail.com>
2023-04-14 19:06:07 +0200

    about-me

--- aboutMe.php
+++ aboutMe.php
@@ -11,21 +11,39 @@ include 'nav.php';
 </header>
 <div class="container mt-4">
     <div class="row">
-        <div id="summary" class="col-12 col-md-4 mb-4">
+        <div id="summary" class="col-12 col-md-3 mb-4">
             <img id="profil" src="/images/jake-nackos-IF9TK5Uy-KI-unsplash-2.jpg" alt="Photo profil" class="img-thumbnail">
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, architecto explicabo! Fuga a, necessitatibus temporibus pariatur praesentium laborum officiis nobis impedit blanditiis perspiciatis, sunt consequatur vel.
+                En résumé...
             </p>
             <ul>
-                <li>Moi 1</li>
-                <li>Moi 2</li>
-                <li>Moi 3</li>
-                <li>Moi 4</li>
+                <li>Je suis coach sportive : j’accompagne toutes les personnes qui souhaitent reprendre une activité physique. </li>
+                <li>Je suis diététicienne : j’accompagne toutes les personnes qui souhaitent apprendre à mieux manger.</li>
+                <li>Je suis fan de lecture, rédactrice et formatrice : j’aime lire, écrire, partager, échanger et apprendre. J’interviens en entreprise et en école de formation.</li>
+                <li>Je suis une rêveuse déterminée, sensible et cyclique passionnée par la vie ❤️ (et super positive mais pas tout le temps… je suis une femme 💪)</li>
             </ul>
         </div>
-        <div class="col-12 col-md-8 mb-4">
-            <h3>Ma présentation</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem nisi repudiandae provident alias sed tempora maiores quasi natus illum? Ipsum voluptate nihil minima mollitia consectetur reprehenderit nisi sed quia doloribus!</p>
+        <div class="col-12 col-md-9 mb-4">
+            <h3>Je suis Julie ☀️ </h3>
+
+            <p> Mariée à un sportif saxophoniste à tendance hyperactive. Il est passionné, doté de beaucoup d’humour et de force mentale incroyable ❤️</p>
+            <p> Maman de deux adorables garçons de 10 et 12 ans, l’un plutôt rêveur et artiste, l’autre plutôt fonceur et determiné 🥰</p>
+            <p>J’ai la chance d’habiter à la campagne 🍀</p>
+            <p>J’ai d’ailleurs régulièrement la visite d’un rouge gorge près de ma baie vitrée et cela me remplit de joie à chaque fois.</p>
+            <p>J’avoue, oui, j’ai un côté Alice aux pays des Merveilles assez prononcé 💫 </p>
+            <p>Je suis fascinée par la vie et par définition, de tout ce qui est vivant et qui génère du mouvement.</p>
+            <p>Je m’intéresse au corps humain et à son fonctionnement. Je trouve incroyable de porter et donner la vie, de vivre sur cette belle planète capable de nous nourrir, nous hydrater. Je suis constamment émerveillée de voir les couchers de soleil, d’entendre les oiseaux siffler, d’être au bord d’une rivière, en haut d’une montagne, de sentir les rayons du soleil réchauffer mon corps et mon cœur ♥️ .</p>
+            <p>Ce corps dont les capacités physiques et mentales me semblent presque infinies. J’ai besoin de comprendre ce dont il a besoin pour être bien, en harmonie, en paix… Loin de tous les « il faut », « je dois » que la société nous impose ou essaie de nous imposer.
+            </p>
+            <br>
+            <p>Alors :</p>
+            <ul>
+                <li>Le sport 🏋️‍♀️ fait partie de moi et de ma vie depuis l’âge de 6 ans : selon moi, c’est l’une des clés essentielles au bien-être physique et mental. Je suis convaincue que le lien social, l’entretien de la force musculaire, de la souplesse et du cœur permettent de mieux vivre et mieux vieillir. </li>
+                <li>L’oxygénation est une autre clé 🔑 : sortir s’aérer, bouger, se connecter à sa respiration, à son corps, tout en déconnectant le cerveau ou au contraire, l’éclaircir en triant, priorisant toutes les choses que l’on aimerait faire. Marcher quotidiennement 10 à 30 min par jour, quelque soit le temps, permet aussi de prendre du temps pour soi.</li>
+                <li>L’alimentation 🥗 est selon moi la base d’un corps qui fonctionne bien. Nous avons besoin de carburant en bonne quantité et de bonne qualité pour révéler les supers pouvoirs de notre corps. </li>
+            </ul>
+            <br>
+            <p>Ah oui, j’allais oublier : j’adore les tulipes 🌷, boire un verre, aller au restau, passer du temps avec ma famille et mes ami.es, mon carré de chocolat avec mon café ☕️, les moments imprévus, danser, écouter de la musique 🎶, la déco et les arts manuels. </p>
         </div>
     </div>
 
@@ -33,13 +51,13 @@ include 'nav.php';
 <div class="container">
     <div class="row row-cols-1 row-cols-md-3 g-4">
         <div class="col">
-            <img src="/images/anupam-mahapatra-Vz0RbclzG_w-unsplash-2.jpg" alt="" class="img-thumbnail">
+            <img src="/images/Lectrice.jpg" alt="" class="img-thumbnail">
         </div>
         <div class="col">
-            <img src="/images/anupam-mahapatra-Vz0RbclzG_w-unsplash-2.jpg" alt="" class="img-thumbnail">
+            <img src="/images/Reveuse.jpg" alt="" class="img-thumbnail">
         </div>
         <div class="col">
-            <img src="/images/anupam-mahapatra-Vz0RbclzG_w-unsplash-2.jpg" alt="" class="img-thumbnail">
+            <img src="/images/sport.jpg" alt="" class="img-thumbnail">
         </div>
     </div>
 </div>
