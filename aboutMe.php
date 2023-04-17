6d9762f4764492e2607e2c22299794c9b2c57c91
Ste[PH]ane <steph.guery@gmail.com>
2023-04-17 19:47:50 +0200

    Test

--- aboutMe.php
+++ aboutMe.php
@@ -47,7 +47,7 @@ include 'nav.php';
 
 </div>
 <div class="container">
-    <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="row row-cols-1 row-cols-md-3 g-5">
         <div class="col">
             <img src="/images/Lectrice.jpg" alt="" class="img-thumbnail">
         </div>
